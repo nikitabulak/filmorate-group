61302bf4dea97675ac8e2d0fa066ec009122913e
nikitabulak <nikitabulak321@yandex.ru>
2022-07-07 21:04:48 +0300

    feat: внесены изменения после код-ревью

--- src/main/java/ru/yandex/practicum/filmorate/service/ReviewService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/ReviewService.java
@@ -1,61 +1,56 @@
 package ru.yandex.practicum.filmorate.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exception.ReviewNotFoundException;
 import ru.yandex.practicum.filmorate.model.Review;
-import ru.yandex.practicum.filmorate.storage.review.ReviewRatingsDao;
 import ru.yandex.practicum.filmorate.storage.review.ReviewStorage;
 
 import java.util.Collection;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class ReviewService {
 
     private final ReviewStorage reviewStorage;
-    private final ReviewRatingsDao reviewRatingsDao;
 
-    public ReviewService(ReviewStorage reviewStorage, ReviewRatingsDao reviewRatingsDao){
-        this.reviewStorage = reviewStorage;
-        this.reviewRatingsDao = reviewRatingsDao;
-    }
-
-    public Collection<Review> getAllReviews(Long filmId, Long count){
+    public Collection<Review> getAllReviews(Long filmId, Long count) {
         return reviewStorage.getAll(filmId, count);
     }
 
-    public Review getReviewById(final Long id){
+    public Review getReviewById(final Long id) {
         return reviewStorage.getById(id).orElseThrow(() ->
                 new ReviewNotFoundException(String.format("Attempt to get review with absent id = %d",
-                       id)));
+                        id)));
     }
 
-    public Review addReview(Review review){
+    public Review addReview(Review review) {
         return reviewStorage.add(review);
     }
 
-    public Review updateReview(Review review){
+    public Review updateReview(Review review) {
         return reviewStorage.update(review);
     }
 
-    public void deleteReviewById(Long id){
+    public void deleteReviewById(Long id) {
         reviewStorage.deleteById(id);
     }
 
-    public void addUserLike(Long id, Long userId){
-        reviewRatingsDao.addLike(id, userId);
+    public void addUserLike(Long id, Long userId) {
+        reviewStorage.addLike(id, userId);
     }
 
-    public void addUserDislike(Long id, Long userId){
-        reviewRatingsDao.addDislike(id, userId);
+    public void addUserDislike(Long id, Long userId) {
+        reviewStorage.addDislike(id, userId);
     }
 
-    public void deleteUserLike(Long id, Long userId){
-        reviewRatingsDao.deleteLike(id, userId);
+    public void deleteUserLike(Long id, Long userId) {
+        reviewStorage.deleteLike(id, userId);
     }
 
-    public void deleteUserDislike(Long id, Long userId){
-        reviewRatingsDao.deleteDislike(id, userId);
+    public void deleteUserDislike(Long id, Long userId) {
+        reviewStorage.deleteDislike(id, userId);
     }
 }
